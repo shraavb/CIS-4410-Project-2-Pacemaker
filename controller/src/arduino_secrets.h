0506251cb5a82c523654f033c97bb79a32febb2a
Shraavasti Bhat <116132803+shraavb@users.noreply.github.com>
2026-07-02 18:14:56 -0700

    security: replace hardcoded credentials with placeholders

--- controller/src/arduino_secrets.h
+++ controller/src/arduino_secrets.h
@@ -2,11 +2,11 @@
 #define ARDUINO_SECRETS_H
 
 // WiFi credentials
-#define SECRET_SSID "AirPennNet-Device"
-#define SECRET_PASS "penn1740wifi"
+#define SECRET_SSID "your_wifi_ssid"
+#define SECRET_PASS "your_wifi_password"
 
 // MQTT credentials
-#define SECRET_MQTT_USERNAME "heart_racer"
+#define SECRET_MQTT_USERNAME "your_mqtt_username"
 #define SECRET_MQTT_PASSWORD ""
 
 #endif
